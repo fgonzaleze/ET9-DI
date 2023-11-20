59a3c527427b23733868eaaa53b3dd7ea0eeeb1c
Javier Gonzalez <jgonzaleze@iesnervion.es>
2023-11-20 21:23:48 +0100

    Hasta el 4 hecho

---
+++ E4T9/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace E4T9
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
---
+++ E4T9/Viewmodels/clsViewModelTroleo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E4T9.Viewmodels
+{
+    
+    public class clsViewModelTroleo : INotifyPropertyChanged
+    {
+
+        // Atributos privados que almacenarán el nombre y el apellido
+        #region atributos
+        private string nombre;
+        private string apellidos;
+        #endregion
+
+        #region constructores
+        public clsViewModelTroleo() { } // Constructor sin parámetros
+        public clsViewModelTroleo(string nombre, string apellidos) // Constructor con el nombre y el apellido
+        {
+            this.apellidos = apellidos;
+            this.nombre = nombre;
+        }
+        #endregion
+
+        #region propiedades
+        public string Nombre
+        {
+            get { return nombre; }
+            set
+            {
+                nombre = value;
+
+                if (nombre.Contains("n")) // Si el nombre tiene n
+                {
+                    apellidos = string.Empty; // Vaciamos el apellido
+                    OnPropertyChanged("Apellidos");
+                }
+            }
+        }
+        public string Apellidos
+        {
+            get { return apellidos; }
+            set
+            {
+                apellidos = value;
+                if (apellidos.Contains("n")) // Si el apellido contiene la n
+                {
+                    nombre = string.Empty; // Vaciamos el nombre
+                    OnPropertyChanged("Nombre");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string atributo = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(atributo));
+        }
+        #endregion
+    }
+
+}
